6f3c909afd5d12428fd7165e747ebcda1ac14a44
Mohamed Inthikhaff <mhdinthikaff@gmail.com>
2025-05-02 18:22:13 +0530

    Refactor App component: remove unused imports and comments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+/* 
 
-function App() {
+can write export default function App() { directly like this or can give export default App() { and then write the function below it.
+
+export default function App() {  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>I am from App Component</h1>  
+    </div>
+  );
+} */
+
+
+
+function App() {  
+  return (
+    <div className="App">
+      <h1>I am from App Component</h1>  
     </div>
   );
 }
