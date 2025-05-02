22c480127f54e298620681eafb2fc43c83da8ee7
Mohamed Inthikhaff <mhdinthikaff@gmail.com>
2025-05-02 18:22:32 +0530

    Refactor index.js: update rendering structure and add example usage of React.Fragment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.js'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ const root = ReactDOM.createRoot(document.getElementById('root'))
+
+ root.render(
+/*   <>
+  <h1>Hello World!</h1>
+  <h2>Welcome to React</h2>
+  <h3>Let's build something amazing together!</h3>
+  <h4>Happy Coding!</h4>  
+  <App />
+  </> */
+
+  // React Fragment is used to group multiple elements without adding extra nodes to the DOM
+
+  <React.Fragment>
+  <h1>Hello World!</h1>
+  <h2>Welcome to React</h2>
+  <h3>Let's build something amazing together!</h3>
+  <h4>Happy Coding!</h4>
+  <App />
+  <App />  {/*  using same component twice to show that we can use the same component multiple times */}
+  </React.Fragment>
+ )
