d3c0d208318b3fb6a0f3b6327a110efe048bb151
Mohamed Inthikhaff <mhdinthikaff@gmail.com>
2025-05-03 22:09:56 +0530

    Remove commented-out class component example from App.js

--- src/App.js
+++ src/App.js
@@ -21,3 +21,23 @@ function App() {
 }
 
 export default App;
+
+
+/* 
+
+Writting class component in react
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {  
+    return (
+      <div className="App">
+        <h1>I am from App Component</h1>  
+      </div>
+    );
+  }
+}
+export default App;
+
+*/
