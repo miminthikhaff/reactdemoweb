34820149e4db38baa38254f2802e0eed76603cb8
Mohamed Inthikhaff <mhdinthikaff@gmail.com>
2025-05-07 22:51:23 +0530

    Add Company component for displaying company details

---
+++ src/components/company/Company.js
@@ -0,0 +1,12 @@
+function company(props) {
+    console.log(props);
+    
+    return (
+      <div className="company">
+        <h1>Company Name:  </h1>
+     
+      </div>
+    );
+  }
+  
+  export default company; 
