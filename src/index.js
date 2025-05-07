96b1627f3b0a7f895f8b577a19504f086730c182
Mohamed Inthikhaff <mhdinthikaff@gmail.com>
2025-05-07 15:42:32 +0530

    Refactor index.js to clean up commented code and improve readability

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.js';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.js";
+import "./index.css";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
- const root = ReactDOM.createRoot(document.getElementById('root'))
-
- root.render(
-/*   <>
+root.render(
+  /*   <>
   <h1>Hello World!</h1>
   <h2>Welcome to React</h2>
   <h3>Let's build something amazing together!</h3>
@@ -18,11 +17,6 @@ import './index.css';
   // React Fragment is used to group multiple elements without adding extra nodes to the DOM
 
   <React.Fragment>
-  <h1>Hello World!</h1>
-  <h2>Welcome to React</h2>
-  <h3>Let's build something amazing together!</h3>
-  <h4>Happy Coding!</h4>
-  <App />
-  <App />  {/*  using same component twice to show that we can use the same component multiple times */}
+    <App />
   </React.Fragment>
- )
+);
