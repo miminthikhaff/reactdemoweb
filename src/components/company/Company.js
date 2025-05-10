622f81553f146d674eb76736c8977020a2c9e9fc
Mohamed Inthikhaff <mhdinthikaff@gmail.com>
2025-05-10 23:23:09 +0530

    Refactor Company component to utilize props for dynamic rendering of company name and details

--- src/components/company/Company.js
+++ src/components/company/Company.js
@@ -1,12 +1,29 @@
 function company(props) {
-    console.log(props);
-    
-    return (
-      <div className="company">
-        <h1>Company Name:  </h1>
-     
-      </div>
-    );
-  }
-  
-  export default company; 
+  // console.log(props);
+
+const { companyName = 'Unknown Company = ', details = 'No details available about the company', children } = props;
+
+  return (
+    <div className="company">
+      <h1>Company Name: {companyName} </h1>
+      <p>{details}</p>
+      { children} 
+    </div>
+  );
+}
+
+// The above code is a functional component in React that takes props as an argument and returns a JSX element.
+// The component is named "company" and it displays the company name and details passed to it as props.
+// The component is then exported as the default export of the module, allowing it to be imported and used in other files.
+// The component is styled with a class name "company" which can be used to apply CSS styles.
+// The component is a simple example of how to create a reusable component in React that can accept props and render dynamic content.
+
+/* company.defaultProps = {
+  companyName: "Unknown Company",
+  details: "No details available about the company",
+}; */
+
+// The above code sets default values for the props of the company component.
+// If the component is used without passing any props, it will display the default values.  
+
+export default company;
