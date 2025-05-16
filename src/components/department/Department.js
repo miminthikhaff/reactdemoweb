839161c6635d06f7c3572881ba4842a2b03d9dee
Mohamed Inthikhaff <mhdinthikaff@gmail.com>
2025-05-16 20:37:23 +0530

    Add Department component to display a list of departments

---
+++ src/components/department/Department.js
@@ -0,0 +1,17 @@
+
+
+const Department = () => {
+  return (
+    <div>
+        <ol>
+            <li>HR</li>
+            <li>Finance</li>
+            <li>IT</li> 
+            <li>Marketing</li>
+            <li>Sales</li>
+        </ol>
+    </div>
+  )
+}
+
+export default Department
