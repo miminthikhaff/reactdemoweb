d7dabebcd327721479c6771158053ab908569e23
Mohamed Inthikhaff <mhdinthikaff@gmail.com>
2025-05-17 15:06:15 +0530

    Refactor Department component to accept departments prop and render dynamically

--- src/components/department/Department.js
+++ src/components/department/Department.js
@@ -1,17 +1,23 @@
 
+import './Department.css';
 
-const Department = () => {
+const Department = ({ departments, children }) => {
+  if (!Array.isArray(departments)) {
+    return <p>No departments available.</p>;
+  }
+
   return (
     <div>
-        <ol>
-            <li>HR</li>
-            <li>Finance</li>
-            <li>IT</li> 
-            <li>Marketing</li>
-            <li>Sales</li>
-        </ol>
+      {children}
+      <ol className="list">
+        {departments.map((item, index) => (
+          <li key={index}>
+            <button className="btn-sm">{item}</button>
+          </li>
+        ))}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default Department
+export default Department;
