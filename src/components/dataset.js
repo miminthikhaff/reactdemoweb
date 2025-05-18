ad6dac992298557c6c51a86568c632c92707ac1d
Mohamed Inthikhaff <mhdinthikaff@gmail.com>
2025-05-18 23:43:59 +0530

    Add companies dataset for dynamic rendering of Company instances

---
+++ src/components/dataset.js
@@ -0,0 +1,21 @@
+export const companies = [
+    {
+        id: 1,
+        companyName: "Google",
+        details: "You can search anything",
+        departments: ["HR", "Finance", "IT", "Marketing"],
+    },
+    {
+        id: 2,
+        companyName: "Microsoft",
+        details: "You can explore anything",
+        departments: ["HR", "Finance", "IT", "Marketing"],
+    },
+    {
+        id: 3,
+        companyName: "Meta",
+        details: "You can connect with anyone",
+        departments: ["HR", "Finance", "IT", "Marketing"],
+    },
+  
+];
